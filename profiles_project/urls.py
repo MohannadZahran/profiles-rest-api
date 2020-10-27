48026334c85850a4aa41fcc37585c6ba47b443b7
Mohannad Zahran <mm-zahran@hotmailcom>
2020-10-27 17:30:52 +0300

    Enable Django admin for user profiles model

--- profiles_project/urls.py
+++ profiles_project/urls.py
@@ -3,6 +3,6 @@ from profiles_api import views
 from django.contrib import admin
 
 urlpatterns = [
-    path('admin'/, admin.site.urls),
+    path('admin/', admin.site.urls),
     path('api/', include('profiles_api.urls'))
 ]
