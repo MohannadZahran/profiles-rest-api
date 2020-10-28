dd2abc68c64c7217c3a95a7ad8707390d91fa02f
Mohannad Zahran <mm-zahran@hotmailcom>
2020-10-28 09:46:14 +0300

    Created a simple Viewset

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-
+from rest_framework import viewsets
 from profiles_api import serializers
 
 class HelloApiView(APIView):
@@ -44,3 +44,16 @@ class HelloApiView(APIView):
     def delete(self, request, pk=None):
         """Delete an opject"""
         return Response({'mothed': 'DELETE'})
+
+class HelloViewSet(viewsets.ViewSet):
+    """Test API ViewSet"""
+
+    def list(self, request):
+        """Return a hello message."""
+        a_viewset = [
+            'Uses actions (list, create, retrieve, update, partial_update)',
+            'Automatically maps to URLS using Routers',
+            'Provides more functionality with less code',
+        ]
+
+        return Response({'message': 'Hello!', 'a_viewset': a_viewset})
